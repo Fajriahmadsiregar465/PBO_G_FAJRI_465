c404420affe6448bfa762eb58af637232fcc2abb
Fajriahmadsiregar465 <sikeren989@gmail.com>
2025-03-15 13:32:05 +0700

    Main.java

--- CODELAB/MODUL2/codelab1/src/Main.java
+++ CODELAB/MODUL2/codelab1/src/Main.java
@@ -3,14 +3,14 @@ class Hewan {
     String jenis;
     String suara;
 
-    // Constructor
+    
     public Hewan(String nama, String jenis, String suara) {
         this.nama = nama;
         this.jenis = jenis;
         this.suara = suara;
     }
 
-    // Metode untuk menampilkan informasi hewan
+    
     public void tampilkanInfo() {
         System.out.println("Nama: " + nama);
         System.out.println("Jenis: " + jenis);
@@ -21,7 +21,7 @@ class Hewan {
 
 public class Main {
     public static void main(String[] args) {
-        // Membuat objek Hewan
+      
         Hewan hewan1 = new Hewan("Kucing", "Mamalia", "Nyann~~");
         Hewan hewan2 = new Hewan("Anjing", "Mamalia", "Woof-Woof!!");
 
